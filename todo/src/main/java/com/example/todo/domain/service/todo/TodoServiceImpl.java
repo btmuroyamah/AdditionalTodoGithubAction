3486d92ec96e1e5e11d4c6bb91d666a265b90ca1
btiwaiharaa <Akira.Iwaihara.bp@nttdata.com>
2020-06-30 11:52:26 +0900

    rest実装

--- todo/src/main/java/com/example/todo/domain/service/todo/TodoServiceImpl.java
+++ todo/src/main/java/com/example/todo/domain/service/todo/TodoServiceImpl.java
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.terasoluna.gfw.common.exception.BusinessException;
 import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
-import org.terasoluna.gfw.common.message.ResultMessage;
 import org.terasoluna.gfw.common.message.ResultMessages;
 
 import com.example.todo.domain.model.Todo;
 import com.example.todo.domain.repository.todo.TodoRepository;
 
 @Service
+@Transactional
 public class TodoServiceImpl implements TodoService {
 
     private static final long MAX_UNFINISHED_COUNT = 5;
@@ -24,19 +24,20 @@ public class TodoServiceImpl implements TodoService {
     @Inject
     TodoRepository todoRepository;
 
-    private Todo findOne(String todoId) {
+    @Override
+    @Transactional(readOnly = true)
+    public Todo findOne(String todoId) {
         Todo todo = todoRepository.findById(todoId).orElse(null);
         if (todo == null) {
             ResultMessages messages = ResultMessages.error();
-            messages.add(ResultMessage
-                    .fromText("[E404] The requested Todo is not found. (id="
-                            + todoId + ")"));
+            messages.add("E404", todoId);
             throw new ResourceNotFoundException(messages);
         }
         return todo;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Collection<Todo> findAll() {
         return todoRepository.findAll();
     }
@@ -46,9 +47,7 @@ public class TodoServiceImpl implements TodoService {
         long unfinishedCount = todoRepository.countByFinished(false);
         if (unfinishedCount >= MAX_UNFINISHED_COUNT) {
             ResultMessages messages = ResultMessages.error();
-            messages.add(ResultMessage
-                    .fromText("[E001] The count of un-finished Todo must not be over "
-                            + MAX_UNFINISHED_COUNT + "."));
+            messages.add("E001", MAX_UNFINISHED_COUNT);
             throw new BusinessException(messages);
         }
 
@@ -72,9 +71,7 @@ public class TodoServiceImpl implements TodoService {
         Todo todo = findOne(todoId);
         if (todo.isFinished()) {
             ResultMessages messages = ResultMessages.error();
-            messages.add(ResultMessage
-                    .fromText("[E002] The requested Todo is already finished. (id="
-                            + todoId + ")"));
+            messages.add("E002", todoId);
             throw new BusinessException(messages);
         }
         todo.setFinished(true);
