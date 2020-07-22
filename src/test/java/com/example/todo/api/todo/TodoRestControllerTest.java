d4d26edbbe6929160cfcaf62febf938636d36ee3
unknown <Akira.Iwaihara.bp@nttdata.com>
2020-07-22 17:05:24 +0900

    TodoRestControllerTestに不正なデータの観点を追加

--- src/test/java/com/example/todo/api/todo/TodoRestControllerTest.java
+++ src/test/java/com/example/todo/api/todo/TodoRestControllerTest.java
@@ -1,6 +1,10 @@
 package com.example.todo.api.todo;
 
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
@@ -25,6 +29,7 @@ import org.mockito.junit.MockitoRule;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.example.todo.domain.model.Todo;
 import com.example.todo.domain.service.todo.TodoService;
@@ -53,7 +58,7 @@ public class TodoRestControllerTest {
 	}
 	
 	@Test
-	public void testgetTodosByLimit_1() throws Exception {
+	public void testgetTodosByLimit_start20200801でend20201231の場合() throws Exception {
 		
 		//検証するデータを登録
 		Collection<Todo> todos = new ArrayList<Todo>();
@@ -69,6 +74,54 @@ public class TodoRestControllerTest {
 		.andExpect(status().isOk())
 		.andExpect(content().contentType("application/json;charset=UTF-8"))
 		.andExpect(content().json("[{\"todoId\": \"1\",\"todoTitle\": \"title1\",\"finished\": false,\"createdAt\": null,\"deadLine\": \"2020-08-01\"}]"));
-		}
 	}
+	
+	@Test
+	public void testgetTodosByLimit_startに不正な値が来た場合() throws Exception {
+		
+		//検証するデータを登録
+		Collection<Todo> todos = new ArrayList<Todo>();
+		Todo todo = new Todo("1", "title1", false, LocalDate.of(2020, 8, 1));
+		todos.add(todo);
+		when(todoService.findByLimit(Mockito.any(), Mockito.any())).thenReturn(todos);
+			mockMvc.perform(
+					get("/todos")
+					.param("start", "日付")
+					.param("end", "2020-12-31")
+					)
+			.andExpect(status().is4xxClientError());
+	}
+	
+	@Test
+	public void testgetTodosByLimit_endに不正な値が来た場合() throws Exception {
+		
+		//検証するデータを登録
+		Collection<Todo> todos = new ArrayList<Todo>();
+		Todo todo = new Todo("1", "title1", false, LocalDate.of(2020, 8, 1));
+		todos.add(todo);
+		when(todoService.findByLimit(Mockito.any(), Mockito.any())).thenReturn(todos);
+			mockMvc.perform(
+					get("/todos")
+					.param("start", "2020-08-01")
+					.param("end", "日付")
+					)
+			.andExpect(status().is4xxClientError());
+	}
+	
+	@Test
+	public void testgetTodosByLimit_startとendに不正な値が来た場合() throws Exception {
+		
+		//検証するデータを登録
+		Collection<Todo> todos = new ArrayList<Todo>();
+		Todo todo = new Todo("1", "title1", false, LocalDate.of(2020, 8, 1));
+		todos.add(todo);
+		when(todoService.findByLimit(Mockito.any(), Mockito.any())).thenReturn(todos);
+			mockMvc.perform(
+					get("/todos")
+					.param("start", "日付")
+					.param("end", "日付")
+					)
+			.andExpect(status().is4xxClientError());
+	}
+}
 	
